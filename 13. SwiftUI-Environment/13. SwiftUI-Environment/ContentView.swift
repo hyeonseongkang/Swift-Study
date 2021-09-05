4fc3f6f9dcc43c535fe13125b11628ea04d5aecb
hyeonseongkang <gt9697@naver.com>
2021-09-05 13:25:37 +0900

    13. Environment 설명 추가

---
+++ 13. SwiftUI-Environment/13. SwiftUI-Environment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  13. SwiftUI-Environment
+//
+//  Created by 강현성 on 2021/09/05.
+//  Copyright © 2021 Mirror. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
