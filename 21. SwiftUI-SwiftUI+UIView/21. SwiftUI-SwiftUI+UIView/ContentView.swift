b6a94d2e8a265742b4017e00fe15fb1a70672df4
hyeonseongkang <gt9697@naver.com>
2021-09-16 17:11:57 +0900

    설명파일 추가

---
+++ 21. SwiftUI-SwiftUI+UIView/21. SwiftUI-SwiftUI+UIView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  21. SwiftUI-SwiftUI+UIView
+//
+//  Created by 강현성 on 2021/09/16.
+//  Copyright © 2021 Mirror. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
